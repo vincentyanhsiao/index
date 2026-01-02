b8e0c3edace26b67fcdcaed96dfc75582edfa528
vincentyanhsiao <vincent.yan.hsiao@gmail.com>
2026-01-02 22:09:55 +0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -151,7 +151,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
